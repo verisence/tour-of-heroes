6d8fa338479494431326c5abf4214541d55208a4
Phil Kipkemboi <kipkemboiphil@gmail.com>
2019-04-24 17:04:12 +0300

    Display the hero details only when the hero is clicked

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Mr. Nice'},
+    {id: 12, name: 'Narco'},
+    {id: 13, name: 'Bombasto'},
+    {id: 14, name: 'Celeritas'},
+    {id: 15, name: 'Magenta'},
+    {id: 16, name: 'Kaliuntu'},
+    {id: 17, name: 'Dyna'},
+    {id: 18, name: 'Verique'},
+    {id: 19, name: 'Nagmano'},
+    {id: 20, name: 'Twister'}
+]
