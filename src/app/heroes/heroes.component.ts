47409b7c7e9045df3a3bbb2a82773fd6ead64ee4
Phil Kipkemboi <kipkemboiphil@gmail.com>
2019-04-25 09:47:46 +0300

    simulate asynchronous access to get hero data

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,10 +10,15 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent implements OnInit {
   selectedHero: Hero;
 
-  heroes = HEROES;
-  constructor() { }
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes=>this.heroes = heroes);
+  }
+
+  heroes : Hero[];
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
